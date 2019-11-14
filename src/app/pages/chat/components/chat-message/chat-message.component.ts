d113dae679d476d055b42a194f587940194ff901
NeeruRamesh <niranjan.mr99@outlook.com>
2019-11-14 07:42:38 +0530

    created chatroom page layout and updated the same

--- src/app/pages/chat/components/chat-message/chat-message.component.ts
+++ src/app/pages/chat/components/chat-message/chat-message.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Message } from './../../../../classes/message';
+import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-chat-message',
   templateUrl: './chat-message.component.html',
   styleUrls: ['./chat-message.component.scss']
 })
 export class ChatMessageComponent implements OnInit {
+
+  @Input() message: Message;
 
   constructor() { }
 
