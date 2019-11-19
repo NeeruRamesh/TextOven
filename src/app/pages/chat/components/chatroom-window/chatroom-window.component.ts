ded54a774fd3b928986cbdafcaa323b43491a391
NeeruRamesh <niranjan.mr99@outlook.com>
2019-11-19 11:45:52 +0530

    updated chatroom routing. Removed dummy data. Get messages from firebase.

--- src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
+++ src/app/pages/chat/components/chatroom-window/chatroom-window.component.ts
@@ -1,54 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { ChatroomService } from 'src/app/services/chatroom.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-chatroom-window',
   templateUrl: './chatroom-window.component.html',
   styleUrls: ['./chatroom-window.component.scss']
 })
-export class ChatroomWindowComponent implements OnInit {
+export class ChatroomWindowComponent implements OnInit, OnDestroy {
 
-  public dummyData = [
-    {
-      message: 'Sed enim velit, condimentum nec tincidunt non, elementum sed nisi.',
-      createdAt: new Date(),
-      sender: {
-        firstName: 'Steve',
-        lastName: 'Smith',
-        photoUrl: 'http://via.placeholder.com/50x50'
-      }
-    },
-    {
-      message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
-      createdAt: new Date(),
-      sender: {
-        firstName: 'Bob',
-        lastName: 'Anderson',
-        photoUrl: 'http://via.placeholder.com/50x50'
-      }
-    },
-    {
-      message: 'Ut eu elit sodales leo ultricies pulvinar. Fusce iaculis magna gravida tempus congue. Ut sit amet nulla sed nisi cursus mattis quis at lacus. Proin commodo, justo in elementum scelerisque, sem urna vulputate enim, ac posuere purus diam ac velit. Sed enim velit, condimentum nec tincidunt non, elementum sed nisi. Cras pharetra dui eu scelerisque pharetra. Curabitur auctor feugiat nibh eget molestie. Duis scelerisque auctor mi, sit amet efficitur magna vulputate quis. Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero. Praesent eu tristique felis. Nunc vestibulum enim et justo dignissim lacinia nec et diam.',
-      createdAt: new Date(),
-      sender: {
-        firstName: 'Sally',
-        lastName: 'Jones',
-        photoUrl: 'http://via.placeholder.com/50x50'
-      }
-    },
-    {
-      message: 'Quisque ornare dapibus convallis. Nam tempor dui a nisl lobortis, sed gravida lectus laoreet. Nullam ornare dui magna. Duis in nisi libero.',
-      createdAt: new Date(),
-      sender: {
-        firstName: 'Sally',
-        lastName: 'Jones',
-        photoUrl: 'http://via.placeholder.com/50x50'
-      }
-    }
-  ];
+  private subscriptions: Subscription[] = [];
+  public chatroom: Observable<any>;
+  public messages: Observable<any>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private route: ActivatedRoute,
+    private chatroomService: ChatroomService,
+    private loadingService: LoadingService
+  ) {
+    this.subscriptions.push(
+      this.chatroomService.selectedChatroom.subscribe(chatroom => {
+        this.chatroom = chatroom;
+      })
+    );
+    this.subscriptions.push(
+      this.chatroomService.selectedChatroomMessages.subscribe(messages => {
+        this.messages = messages;
+      })
+    );
   }
 
+  ngOnInit() {
+    this.subscriptions.push(
+      this.route.paramMap.subscribe(params => {
+        const chatroomId = params.get('chatroomId');
+        this.chatroomService.changeChatroom.next(chatroomId);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe());
+  }
 }
--- src/app/services/chatroom.service.ts
+++ src/app/services/chatroom.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { LoadingService } from './loading.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +9,26 @@ import { AngularFirestore, AngularFirestoreDocument } from 'angularfire2/firesto
 export class ChatroomService {
 
   public chatrooms: Observable<any>;
+  public changeChatroom: BehaviorSubject<string | null> = new BehaviorSubject(null);
+  public selectedChatroom: Observable<any>;
+  public selectedChatroomMessages: Observable<any>;
 
   constructor(
-    private db: AngularFirestore
+    private db: AngularFirestore,
+    private loadingService: LoadingService
   ) {
+    this.selectedChatroom = this.changeChatroom.switchMap(chatroomId => {
+      if (chatroomId) {
+        return db.doc(`chatrooms/${chatroomId}`).valueChanges();
+      }
+      return Observable.of(null);
+    });
+    this.selectedChatroomMessages = this.changeChatroom.switchMap(chatroomId => {
+      if (chatroomId) {
+        return db.collection(`chatrooms/${chatroomId}/messages`).valueChanges();
+      }
+      return Observable.of(null);
+    })
     this.chatrooms = db.collection('chatrooms').valueChanges();
   }
 }
